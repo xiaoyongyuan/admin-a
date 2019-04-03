4197f17ad0320b9019c6ff227458aea9fe326935
azj7319 <568963700@qq.com>
2019-04-03 11:23:34 +0800

    父组件调用子组件

--- src/components/common/CascaderModule.js
+++ src/components/common/CascaderModule.js
@@ -22,16 +22,26 @@ class CascaderModule extends Component {
         };
     }
     componentDidMount() {
-        this.getsData("/api/zonecode/gets_province",{},'province_id','province_name',true,1).then((res)=>{
+        var params={
+          url:"/api/zonecode/gets_province",
+          data:{},
+          id:'province_id',
+          name:'province_name',
+          sub:true,
+          grade:0
+        }
+        this.getsData(params).then((res)=>{
           if(res) this.setState({options:res});
         })
+        this.props.onRef(this)
+
     }
-    getsData=(url,data,province_id,province_name,sub,grade)=>{
+    getsData=(params)=>{
       const _this=this;
       return new Promise((resolve,reject)=>{
-        post({url:url,data:data}, (res)=>{ //省
+        post({url:params.url,data:params.data}, (res)=>{ //省
           if(res.success){
-            resolve(_this.forEachData(res.data,province_id,province_name,sub,grade))
+            resolve(_this.forEachData(res.data,params.id,params.name,params.sub,params.grade))
           }else reject(false)
         })
       })
@@ -45,37 +55,46 @@ class CascaderModule extends Component {
     };
     onChange = (value, selectedOptions) => {
       this.setState({
-        zonecode:value[value.length-1],
+        zonecode:value.length?value[value.length-1]:'',
         usertype:value.length-1
-      })
+      })   
     }
     loadData = (selectedOptions) => {
       const targetOption = selectedOptions[selectedOptions.length - 1];
-      targetOption.loading = true
-      if(targetOption.grade==1){
-        this.getsData("/api/zonecode/gets_city",{province_id:targetOption.value},'city_id','city_name',true,2).then((res)=>{
-          if(res){
-            targetOption.loading = false;
-            targetOption.children=res;
-            this.setState({
-              options: [...this.state.options],
-            });
-          }
-        })
-      }else if(targetOption.grade==2){
-        this.getsData("/api/zonecode/gets_county",{city_id:targetOption.value},'county_id','county_name',false,3).then((res)=>{
-          if(res){
-            targetOption.loading = false;
-            targetOption.children=res;
-            this.setState({
-              options: [...this.state.options],
-            });
-          }
-        })
+      targetOption.loading = true;
+      var params={};
+      if(targetOption.grade==0){
+        params={
+          url:"/api/zonecode/gets_city",
+          data:{province_id:targetOption.value},
+          id:'city_id',
+          name:'city_name',
+          sub:true,
+          grade:1
+        }
+      }else if(targetOption.grade==1){
+        params={
+          url:"/api/zonecode/gets_county",
+          data:{city_id:targetOption.value},
+          id:'county_id',
+          name:'county_name',
+          sub:false,
+          grade:2
+        }
       }
+      
+      this.getsData(params).then((res)=>{
+        if(res){
+          targetOption.loading = false;
+          targetOption.children=res;
+          this.setState({
+            options: [...this.state.options],
+          });
+        }
+      })
 
     }
-    forEachData=(data,id,name,sub,grade)=>{
+    forEachData=(data,id,name,sub,grade)=>{ //循环改造数组为需要的格式，grade为省市区等级，sub为可否有下一级
       var neaData=[]
       data.map((el,i)=>{
         var ex={
