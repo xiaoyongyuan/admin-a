f1a636514a03afacc7a292e14529c5b89e72c1b0
shgg <547537959@qq.com>
2019-04-04 13:17:22 +0800

    一次报警页面加搜索

--- src/components/alarmsta/selectDev.js
+++ src/components/alarmsta/selectDev.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Icon,message,Spin} from "antd";
+import {Icon,message,Spin,Input} from "antd";
 import "../../style/ztt/css/police.css";
 import "../../style/publicStyle/publicStyle.css";
 import 'moment/locale/zh-cn';
@@ -15,6 +15,7 @@ class selectDev extends React.Component{
             isrequest:true,//是否请求接口
             loading:true,//加载中的状态
             load:false,//右边加载中的状态
+            kong:false,//左边是否为空
         };
     }
     componentDidMount() {
@@ -30,6 +31,8 @@ class selectDev extends React.Component{
             var clientHeight = document.getElementById("scorll").clientHeight;//div内里框框的高度
             var scrollbottom=scrollHeight-clientHeight;
             var scrollTopP=Math.floor(scrollTop);
+            console.log('scrollbottom-scrollTopP',scrollbottom-scrollTopP);
+            
             if(scrollbottom-scrollTopP===0){//滚动到底部了
                 if(_this.state.isrequest){ 
                         _this.setState({
@@ -97,41 +100,85 @@ class selectDev extends React.Component{
         }
         post({url:'/api/equipment/getlistforadmin',data:params},(res)=>{
             if(res){
-                this.setState({
-                    load:false,
-                    listequmpent:res.data,
-                    totaleq:res.totalcount,
-                }); 
+                if(this.state.kong){
+                    this.setState({
+                        listequmpent:[],
+                        load:false,
+                    }); 
+                }else{
+                    this.setState({
+                        load:false,
+                        listequmpent:res.data,
+                        totaleq:res.totalcount,
+                    }); 
+                }
+                 
             }   
         })
-
     }
-    unitcode=(code) =>{ //点击获取设备code
+    unitcode=(code,cnamee) =>{ //点击获取设备code
+        console.log('admincode',code,cnamee);
         this.setState({
             unitcode: code,
+            cnamee:cnamee
         },()=>{
             this.requestequmpent();
         });
     }
-
+    myonSearch=(e) =>{ //点击获取设备code
+        console.log('myonSearch',e.target.value);
+        let searchvalue=e.target.value
+        this.setState({ loading:true })
+        const Searchdata={cname:searchvalue,}
+        post({url:"/api/company/getlist_user",data:Searchdata}, (res)=>{
+            if(res.success){
+                console.log('*(res.data.length',res.data.length);
+                this.setState({
+                    list: res.data,
+                    total:res.totalcount,
+                    loading:false,
+                })
+                if(res.data.length){
+                    this.setState({
+                        unitcode:res.data[0].code,
+                        kong:false,
+                    },()=>{
+                        this.requestequmpent();
+                    })
+                }else{
+                    this.setState({
+                        kong:true,
+                    },()=>{
+                        this.requestequmpent();
+                    })
+                }
+            }
+        })
+    }
     render(){
         return(
             <div className="selectDev">
                 <div className="selectcontent">
-                    <div className="devtittle"><span>已选择:</span><span>1</span>><span>12</span> </div>
+                    <div className="devtittle"><span>已选择 : </span><span> {this.state.cnamee}</span></div>
                     <div className="selectframe">
                         <div className="selectcard">
                             <div className="cardtit">选择单位（{this.state.total}）</div>
+                                    <div className="searcht">  
+                                        <Input placeholder="请输入搜索内容" allowClear onChange={this.myonSearch} />
+                                    </div>
                                 <div className="cardbody"id="scorll">
                                     <Spin spinning={this.state.loading} size="large" className="spin" tip="加载中..." >
+                                    <div>
+                                    
                                         {
                                         this.state.list.map((item)=>(
-                                            <div key={item.code} className="cardbodyitem"onClick={ ()=>this.unitcode(item.code) } >
-                                                <span>{item.adminname}</span>
+                                            <div key={item.code} className="cardbodyitem" onClick={ ()=>this.unitcode(item.code,item.cname) } >
+                                                <span>{item.cname}</span>
                                                 <span></span> 
                                             </div>
                                         ))
                                         }
+                                        </div>
                                     </Spin>
                                 </div>
                         </div>
